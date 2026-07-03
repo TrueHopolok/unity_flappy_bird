a70079444c7958b69c351373fb1d5066c874e00b
TrueHopolok <vadim.truhanov@inbox.lv>
2026-07-03 21:45:09 +0300

    fixed bird jump input

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -3,25 +3,35 @@ using UnityEngine.InputSystem;
 
 public class Bird : MonoBehaviour
 {
-    [SerializeField] float jumpForce = 100.0f;
+    [SerializeField] float jumpForce = 1.0f;
     Rigidbody2D rigidBody;
+    InputControlls controls;
     InputAction jumpAction;
     bool hasJumped = false;
 
-    void Init()
+    void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        jumpAction = InputSystem.actions.FindAction("Jump");
+        controls = new InputControlls();
+        controls.Enable();
+        jumpAction = controls.Player.Jump;
     }
 
     void FixedUpdate()
     {
         // bool hasPressedSpace = Input.GetKeyDown(KeyCode.Space);
         bool hasPressedSpace = jumpAction.IsPressed();
+        Debug.Log(hasPressedSpace);
         if (!hasJumped && hasPressedSpace)
         {
             rigidBody.linearVelocityY = jumpForce;
         }
         hasJumped = hasPressedSpace;
+    }
+
+    void OnDestroy()
+    {
+        controls.Disable();
+        controls.Dispose();       
     }
 }
